6b1f5b072b68c94672fd8bd84c1ca49c6c566c18
F-Moody <federico.cirasa@gmail.com>
2021-04-06 10:14:09 +0200

    added heart model

--- src/script.js
+++ src/script.js
@@ -2,7 +2,12 @@ import './style.css'
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import * as dat from 'dat.gui'
+import {GLTFLoader} from "three/examples/jsm/loaders/GLTFLoader";
+// Loading
 
+const textureLoader = new THREE.TextureLoader()
+
+const normalTexture = textureLoader.load('/textures/NormalMap.png')
 // Debug
 const gui = new dat.GUI()
 
@@ -13,16 +18,28 @@ const canvas = document.querySelector('canvas.webgl')
 const scene = new THREE.Scene()
 
 // Objects
-const geometry = new THREE.TorusGeometry( .7, .2, 16, 100 );
+const geometry = new THREE.SphereBufferGeometry( .5, 64, 64 );
+
+const gltfLoader = new GLTFLoader();
+const url = '/models/heart_made_of_strings/scene.gltf';
+gltfLoader.load(url, (gltf) => {
+    const root = gltf.scene
+    scene.add(root)
+});
+
+
 
 // Materials
 
-const material = new THREE.MeshBasicMaterial()
-material.color = new THREE.Color(0xff0000)
+const material = new THREE.MeshStandardMaterial()
+material.metalness = 0.7
+material.roughness = 0.2
+material.normalMap = normalTexture
+material.color = new THREE.Color(0x292929)
 
 // Mesh
 const sphere = new THREE.Mesh(geometry,material)
-scene.add(sphere)
+// scene.add(sphere)
 
 // Lights
 
@@ -31,6 +48,19 @@ pointLight.position.x = 2
 pointLight.position.y = 3
 pointLight.position.z = 4
 scene.add(pointLight)
+
+const pointLightRed = new THREE.PointLight(0xff0000, 2)
+pointLightRed.position.set(-1.86,1,-1.65)
+pointLightRed.intensity = 10
+scene.add(pointLightRed)
+
+/*gui.add(pointLightRed.position, 'y').min(-3).max(3).step(0.01)
+gui.add(pointLightRed.position, 'x').min(-6).max(6).step(0.01)
+gui.add(pointLightRed.position, 'z').min(-3).max(3).step(0.01)
+gui.add(pointLightRed, 'intensity').min(0).max(10).step(0.01)*/
+
+const pointLightHelper = new THREE.PointLightHelper(pointLightRed,1)
+// scene.add(pointLightHelper)
 
 /**
  * Sizes
@@ -73,10 +103,15 @@ scene.add(camera)
  * Renderer
  */
 const renderer = new THREE.WebGLRenderer({
-    canvas: canvas
+    canvas: canvas,
+    alpha:true
 })
 renderer.setSize(sizes.width, sizes.height)
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+
+const controls = new OrbitControls( camera, renderer.domElement )
+camera.position.set( 0, 10, 10 )
+controls.update()
 
 /**
  * Animate
@@ -94,6 +129,8 @@ const tick = () =>
 
     // Update Orbital Controls
     // controls.update()
+
+    controls.update()
 
     // Render
     renderer.render(scene, camera)
